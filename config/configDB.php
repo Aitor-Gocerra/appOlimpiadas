70297b286aba8c09c08e2dc453feb3435415c864
Aitor Gomez Cerrato <aitorgomezcerrato@gmail.com>
2026-02-02 11:55:23 +0100

    Update configDB.php

--- config/configDB.php
+++ config/configDB.php
@@ -1,13 +1,13 @@
 <?php
-/* // Configuración de la base de datos
-DEFINE('servidor', 'localhost');
-DEFINE('usuario', 'root');
-DEFINE('contraseña', '');
-DEFINE('nombreBaseDatos', 'olimpiadas'); */
+    /* // Configuración de la base de datos
+    DEFINE('servidor', 'localhost');
+    DEFINE('usuario', 'root');
+    DEFINE('contraseña', '');
+    DEFINE('nombreBaseDatos', 'olimpiadas'); */
 
-// DATOS DEL SERVIDOR
-DEFINE('servidor', '05.daw.esvirgua.com');
-DEFINE('usuario', 'daw_userbd05');
-DEFINE('contraseña', '-Xnmq@kDI0]K2dc4');
-DEFINE('nombreBaseDatos', 'daw_05_BD1'); 
+    // DATOS DEL SERVIDOR
+    DEFINE('servidor', '05.daw.esvirgua.com');
+    DEFINE('usuario', 'daw_userbd05');
+    DEFINE('contraseña', '-Xnmq@kDI0]K2dc4');
+    DEFINE('nombreBaseDatos', 'daw_05_BD1'); 
 ?>
